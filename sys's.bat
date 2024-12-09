250d30c566e1f82d7811cc700cbf8db9ee9e7203
Sniffer-Lexel <xzelvo@gmail.com>
2024-12-09 23:34:14 +0530

    Update sys's.bat

--- sys's.bat
+++ sys's.bat
@@ -1,6 +1,3 @@
 @echo off
-rem Navigate to the directory where HD-Adb.exe is located
 cd "C:\Program Files\MSI App Player"
-
-rem Launch Free Fire using HD-Adb.exe
 HD-Adb.exe shell monkey -p com.dts.freefireth -c android.intent.category.LAUNCHER 1
